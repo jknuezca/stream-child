48f86a1251585b9ae75f756cff1d332e1217ce96
jknuezca <nuezcajorge@gmail.com>
2023-08-19 11:34:06 +0400

    Initial Commit

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php 
+    get_header();
+?>
+
+<main class="main-layout"> 
+    <section class="match-list-container">
+        <div id="match-list">
+            <h1>Match List</h1>
+            <!-- Match items will be dynamically inserted here -->
+        </div>
+    </section>
+</main>
+
+
+<?php 
+    get_footer();
+?>
+
